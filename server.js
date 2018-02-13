03b9e932ba6e400009adaf64bd8eae476ef0c4b5
aurorex <selva_beat@hotmail.com>
2018-02-14 00:07:32 +0100

    modificand server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // llamamos a la libreria express
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 // elegimos el puerto en donde va a correr nuestro servidor
 const port = process.env.PORT || 3000;
 // le pasamos como argumento el puerto anterior y dentro del callback colocamos el mensaje que aparecerá en la consola
@@ -8,5 +8,4 @@ app.listen(port, () => {
   console.log('Server running on port ' + port + '!');
 });
 app.use(express.static(__dirname + '/public'));
-
 
